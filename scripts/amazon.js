7a0d0fc95b2d47f88bec673650a4b86fc75a8cb0
Ji_Chris <kisnaanand623@gmail.com>
2024-07-27 06:51:29 +0530

    item get added according to selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -63,6 +63,8 @@ document.querySelectorAll('.js-add-to-cart').forEach(
         button.addEventListener('click',()=>{
             const productId = button.dataset.productId;
             let matchingItem;
+            const toAdd = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+            console.log(toAdd);
 
             cart.forEach((item)=>{
                 if (productId === item.productId){
@@ -75,7 +77,7 @@ document.querySelectorAll('.js-add-to-cart').forEach(
             }else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: toAdd
                 })
                 
             }
